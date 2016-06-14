85c57501316dcd41dec65ad50dc52d7274150cdf
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-06-14 11:59:51 +0200

    construct cmd

--- jobs_in_tandem_with_scripts/jobs.sh
+++ jobs_in_tandem_with_scripts/jobs.sh
@@ -17,7 +17,8 @@ echo "job id of first script is "$jobid
 # Run second script
 ##############################
 
-echo "Sending script to sbatch:"
-jobid=`sbatch job_2.sh --dependecy=afterok:$id | cut -d ' ' -f 4`
+cmd="sbatch job_2.sh --dependecy=afterok:"$"id | cut -d ' ' -f 4"
+echo "Starting cmd: "$cmd
+jobid=`cmd`
 
 echo "job id of second script is "$jobid
