f2d3359f087be960cf226cf0efd7e11d646b3c5a
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-06-14 12:08:33 +0200

    script did not introduce a dependency

--- jobs_in_tandem_with_scripts/jobs.sh
+++ jobs_in_tandem_with_scripts/jobs.sh
@@ -3,12 +3,12 @@
 # Clean up
 rm *.txt
 rm *.log
+echo "Cleaned up"
 
 ##############################
 # Run first script
 ##############################
 
-echo "Sending script to sbatch:"
 jobid=`sbatch job_1.sh | cut -d ' ' -f 4`
 
 echo "job id of first script is "$jobid
@@ -17,7 +17,10 @@ echo "job id of first script is "$jobid
 # Run second script
 ##############################
 
-echo "Sending script to sbatch:"
-jobid=`sbatch job_2.sh --dependency=afterok:$id | cut -d ' ' -f 4`
+cmd="sbatch job_2.sh --dependency=afterok:"$id
+
+echo "cmd: "$cmd
+
+jobid=`$cmd | cut -d ' ' -f 4`
 
 echo "job id of second script is "$jobid
