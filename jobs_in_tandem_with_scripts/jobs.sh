3c4bbb6a0b134357cdda46f864a6e7a42f60e31c
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-06-14 12:00:25 +0200

    corrected identifier

--- jobs_in_tandem_with_scripts/jobs.sh
+++ jobs_in_tandem_with_scripts/jobs.sh
@@ -17,7 +17,7 @@ echo "job id of first script is "$jobid
 # Run second script
 ##############################
 
-cmd="sbatch job_2.sh --dependecy=afterok:"$"id | cut -d ' ' -f 4"
+cmd="sbatch job_2.sh --dependecy=afterok:"$"jobid | cut -d ' ' -f 4"
 echo "Starting cmd: "$cmd
 jobid=`cmd`
 
