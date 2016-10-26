e8da65ab94e78245f87f352f0cce319114b2318f
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-26 14:53:36 +0200

    For Giovanni

---
+++ jobs_in_sequence_with_parameter/job1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=0:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1M
+#SBATCH --job-name=job1
+#SBATCH --output=job1.log
+echo "Created by job1.sh" > out.txt
---
+++ jobs_in_sequence_with_parameter/job2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=0:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1M
+#SBATCH --job-name=job2
+#SBATCH --output=job2.log
+echo "Appended by job2.sh" >> out.txt
+$1
+
