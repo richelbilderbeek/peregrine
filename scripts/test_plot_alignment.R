b6034f3b296be45050216dbe51b0104e37135125
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-15 09:18:58 +0200

    Test https://github.com/richelbilderbeek/razzo/issues/198

---
+++ scripts/test_plot_alignment.R
@@ -0,0 +1,10 @@
+# Test if a DNA alignment is plotted nicely on Peregrine
+library(ape)
+library(phangorn)
+fdir <- system.file("extdata/trees", package = "phangorn")
+primates <- read.phyDat(file.path(fdir, "primates.dna"), format = "interleaved")
+dna_alignment <- primates
+
+png("myfile.png", width = 800, height = 600, res = 144)
+ape::image.DNAbin(x = dna_alignment)
+dev.off()
