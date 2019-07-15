1ebaca66a744f9382f77794dad17c87e6ac8da94
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-15 10:33:56 +0200

    Try ggplot on Peregrine, progress https://github.com/richelbilderbeek/razzo/issues/208

---
+++ scripts/test_plot_ggplot.R
@@ -0,0 +1,17 @@
+# Test if a ggplot2 plot is plotted nicely on Peregrine
+#
+# Works with:
+#
+#   sbatch run_r_script_with_x test_plot_ggplot.R
+#
+
+library(ggplot2)
+
+dat <- data.frame(
+  time = factor(c("Lunch", "Dinner"), levels = c("Lunch", "Dinner")),
+  total_bill = c(14.89, 17.23)
+)
+
+# Very basic bar graph
+ggplot(data = dat, aes(x = time, y = total_bill)) +
+    geom_bar(stat = "identity") + ggsave("ggplot.png", width = 7, height = 7)
