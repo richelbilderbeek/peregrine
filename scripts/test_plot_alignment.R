efd56108ac4a87d82ab4359dfa600db0d15d4f88
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-15 09:50:43 +0200

    Script works locally

--- scripts/test_plot_alignment.R
+++ scripts/test_plot_alignment.R
@@ -3,7 +3,7 @@ library(ape)
 library(phangorn)
 fdir <- system.file("extdata/trees", package = "phangorn")
 primates <- read.phyDat(file.path(fdir, "primates.dna"), format = "interleaved")
-dna_alignment <- primates
+dna_alignment <- as.DNAbin(primates)
 
 png("myfile.png", width = 800, height = 600, res = 144)
 ape::image.DNAbin(x = dna_alignment)
