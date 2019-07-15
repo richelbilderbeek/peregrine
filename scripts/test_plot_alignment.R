151073da79daaba6febe5911425b00a34c9e211a
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-15 09:55:26 +0200

    Alignment can be plotted nicely on Peregrine, fix https://github.com/richelbilderbeek/razzo/issues/198

--- scripts/test_plot_alignment.R
+++ scripts/test_plot_alignment.R
@@ -1,10 +1,16 @@
 # Test if a DNA alignment is plotted nicely on Peregrine
+#
+# Works with:
+#
+#   sbatch run_r_script_with_x test_plot_alignment.R
+#
+
 library(ape)
 library(phangorn)
 fdir <- system.file("extdata/trees", package = "phangorn")
 primates <- read.phyDat(file.path(fdir, "primates.dna"), format = "interleaved")
 dna_alignment <- as.DNAbin(primates)
 
-png("myfile.png", width = 800, height = 600, res = 144)
+png("alignment.png", width = 800, height = 600, res = 144)
 ape::image.DNAbin(x = dna_alignment)
 dev.off()
