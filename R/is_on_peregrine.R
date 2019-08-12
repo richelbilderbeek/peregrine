3723ff673ed3bbffab0428ca27bef8db1e0ad38c
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-12 13:08:12 +0200

    Thanks @lintr-bot

--- R/is_on_peregrine.R
+++ R/is_on_peregrine.R
@@ -3,5 +3,5 @@
 #' @author Richel J.C. Bilderbeek
 #' @export
 is_on_peregrine <- function() {
-  is_on_peregrine_login_node() || is_on_peregrine_worker_node()
+  is_on_peregrine_login_node() || is_on_peregrine_worker_node() # nolint peregrine function
 }
