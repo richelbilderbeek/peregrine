da0c88e0888d7013be0eab8b864929d092b61bc9
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-12 14:23:43 +0200

    Big test [skip ci]

--- scripts/test_is_pff.R
+++ scripts/test_is_pff.R
@@ -35,10 +35,6 @@ can_create_file <- function(filename) {
   file.exists(filename)
 }
 
-print("Can create file?")
-print(" ")
-print("filename | can_create_file")
-print("---|---")
 filenames <- c(
   "/local/tmp.txt",
   "/local/tmp/tmp.txt",
@@ -49,11 +45,16 @@ filenames <- c(
   "/home/p230198/tmp.txt",
   "/home/tmp.txt"
 )
+
+print("Can create file?")
+print(" ")
+print("filename | can_create_file")
+print("---|---")
 for (filename in filenames) {
   print(paste0(filename, " | ", can_create_file(filename)))
 }
 
-
+# Hardcoded results
 if (is_on_peregrine_login_node()) {
   expect_false(can_create_file("/local/tmp.txt"))
   expect_true(can_create_file("/local/tmp/tmp.txt"))
@@ -76,69 +77,47 @@ if (is_on_peregrine_worker_node()) {
   expect_false(can_create_file("/home/tmp.txt"))
 }
 
-stop("OK")
-
-non_pff_prefixes <- NULL
-if (peregrine::is_on_peregrine_worker_node()) {
-  non_pff_prefixes <- c(
-    # "/tmp/"
-    "/",
-    "/data/",
-    "/data/p230198"
+can_run_beast <- function(prefix) {
+  input_filename <- beastier::get_beastier_paths("2_4.xml")
+  output_log_filename <- file.path(prefix, "out.log")
+  output_trees_filenames <- file.path(prefix, "out.trees")
+  output_state_filename <- file.path(prefix, "out.xml.state")
+  beast2_options <- beastier::create_beast2_options(
+    input_filename = input_filename,
+    output_log_filename = output_log_filename,
+    output_trees_filenames = output_trees_filenames,
+    output_state_filename = output_state_filename
   )
-}
-if (peregrine::is_on_peregrine_login_node()) {
-  non_pff_prefixes <- c(
-    "/tmp/",
-    "/",
-    "/data/",
-    "/data/p230198"
+  suppressMessages(
+    beastier::run_beast2_from_options(beast2_options)
   )
-}
-
-# Can create file there?
-for (non_pff_prefix in non_pff_prefixes) {
-  print(paste0("Testing prefix '", non_pff_prefix, "'"))
-  filename <- file.path(non_pff_prefix, "tmp.txt")
-  file.create(filename, showWarnings = FALSE)
-  print(paste0("File '", filename, "' exists: ", file.exists(filename)))
-  # testthat::expect_false(file.exists(filename))
-}
-
-for (non_pff_prefix in non_pff_prefixes) {
-  # testit::assert(!peregrine::is_pff(non_pff_prefix))
-  print(paste0("Testing prefix '", non_pff_prefix, "'"))
-  testthat::expect_error(
-    suppressMessages(
-      beastier::run_beast2_from_options(
-        beastier::create_beast2_options(
-          input_filename = beastier::get_beastier_paths("2_4.xml"),
-          output_log_filename = file.path(non_pff_prefix, "out.log"),
-          output_trees_filenames = file.path(non_pff_prefix, "out.trees"),
-          output_state_filename = file.path(non_pff_prefix, "out.xml.state")
-        )
-      )
-    ),
-    "File 'output_log_filename' not found"
-  )
-}
-
-pff_prefixes <- c(
-  "/home/p230198",
-  "/local/tmp/"
-)
-
-for (pff_prefixes in pff_prefixes) {
-  # testit::assert(peregrine::is_pff(pff_prefix))
-  print(paste0("Testing prefix '", pff_prefix, "'"))
-  testthat::expect_silent(
-    beastier::run_beast2_from_options(
-      beastier::create_beast2_options(
-        input_filename = beastier::get_beastier_paths("2_4.xml"),
-        output_log_filename = file.path(pff_prefix, "tmp_out.log"),
-        output_trees_filenames = file.path(pff_prefix, "tmp_out.trees"),
-        output_state_filename = file.path(pff_prefix, "tmp_out.xml.state")
+  all(
+    file.exists(
+      c(
+        input_filename,
+        output_log_filename,
+        output_trees_filenames,
+        output_state_filename
       )
     )
   )
+}
+
+prefixes <- c(
+  "/local",
+  "/local/tmp",
+  "/tmp",
+  "/",
+  "/data",
+  "/data/p230198",
+  "/home",
+  "/home/p230198"
+)
+
+print("Can create BEAST2?")
+print(" ")
+print("prefix | can_run_beast")
+print("---|---")
+for (prefix in prefixes) {
+  print(paste0(filename, " | ", can_run_beast(filename)))
 }
