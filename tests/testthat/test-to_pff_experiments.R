07c503a53e9fbaafcbcd0569ef0c5e933a9d028e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-20 09:04:34 +0200

    Fix tests from more strict 'pirouette::check_experiments'

--- tests/testthat/test-to_pff_experiments.R
+++ tests/testthat/test-to_pff_experiments.R
@@ -17,6 +17,7 @@ test_that("use, gen + cand", {
     pirouette::create_gen_experiment(),
     pirouette::create_cand_experiment()
   )
+  experiments[[2]]$inference_model$site_model <- create_gtr_site_model()
   expect_false(are_pff_experiments(experiments))
   experiments <- to_pff_experiments(experiments)
   expect_true(are_pff_experiments(experiments))
@@ -29,6 +30,8 @@ test_that("use, gen + cand + cand", {
     cand_experiment,
     cand_experiment
   )
+  experiments[[2]]$inference_model$site_model <- create_gtr_site_model()
+  experiments[[3]]$inference_model$site_model <- create_tn93_site_model()
   expect_false(are_pff_experiments(experiments))
   experiments <- to_pff_experiments(experiments)
   expect_true(are_pff_experiments(experiments))
@@ -40,6 +43,7 @@ test_that("use, cand + cand", {
     cand_experiment,
     cand_experiment
   )
+  experiments[[2]]$inference_model$site_model <- create_gtr_site_model()
   expect_false(are_pff_experiments(experiments))
   experiments <- to_pff_experiments(experiments)
   expect_true(are_pff_experiments(experiments))
