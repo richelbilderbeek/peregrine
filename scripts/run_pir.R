8ae7615df7f3e0cb697f03f006a31b71373038b4
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-27 12:23:26 +0200

    Fix script

--- scripts/run_pir.R
+++ scripts/run_pir.R
@@ -1,5 +1,5 @@
 # R script to test if pirouette runs on peregrine
 pirouette::pir_run(
   phylogeny = ape::read.tree(text = "((A:2, B:2):1, C:3);"),
-  pir_params = create_test_pff_pir_params()
+  pir_params = peregrine::create_test_pff_pir_params()
 )
