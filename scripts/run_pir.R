67e3fa98187bfd9b9a070ee3d3f7c1e9dab3dfed
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-27 12:32:08 +0200

    Fix script, progress #9

--- scripts/run_pir.R
+++ scripts/run_pir.R
@@ -3,16 +3,18 @@
 # Experiments
 gen_exp <- peregrine::create_test_pff_gen_experiment()
 cand_exps <- peregrine::create_all_pff_experiments(
+  mcmc = gen_exp$inference_model$mcmc,
   exclude_model = gen_exp$inference_model
 )
 experiments <- c(list(gen_exp), cand_exps)
-
+peregrine::check_pff_experiments(experiments)
 
 # pirouette parameters
 pir_params <- peregrine::create_test_pff_pir_params(
   experiments = experiments,
   twinning_params = peregrine::create_pff_twinning_params()
 )
+peregrine::check_pff_pir_params(pir_params)
 
 # Run it
 pirouette::pir_run(
