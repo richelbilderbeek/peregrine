0314af4531acb55cd14fa5487f24c8b22c4a4e50
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-30 08:47:11 +0100

    Long tests on Travis only

--- tests/testthat/test-are_pff_experiments.R
+++ tests/testthat/test-are_pff_experiments.R
@@ -1,4 +1,5 @@
 test_that("use", {
+  if (!beastier::is_on_travis()) return()
   expect_false(are_pff_experiments(pirouette::create_all_experiments()))
   expect_true(are_pff_experiments(create_all_pff_experiments()))
 })
