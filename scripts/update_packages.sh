a8fb6f616bde37ce3cfdb9c95fa65d898a53dba1
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-04 10:47:07 +0200

    Add 'update_packages.sh'

---
+++ scripts/update_packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to install razzo and its dependencies
+# on the Peregrine computer cluster
+#
+# Usage, locally:
+#
+#   ./update_packages.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./update_packages.sh
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=update_packages
+#SBATCH --output=update_packages.log
+module load R
+
+update.packages()
