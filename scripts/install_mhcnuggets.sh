0e4642c2d667338f32228b1fa132882278ab4f77
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-28 10:11:24 +0200

    Install MHCnuggets and mhcnuggetsr

---
+++ scripts/install_mhcnuggets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to install MHCnuggets
+# on the Peregrine computer cluster
+#
+# Usage, locally:
+#
+#   ./install_mhcnuggets.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./install_mhcnuggets.sh
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=install_mhcnuggets
+#SBATCH --output=install_mhcnuggets.log
+module load R
+
+Rscript -e 'if (!mhcnuggetsr::is_mhcnuggets_installed()) mhcnuggetsr::install_mhcnuggets()'
+
---
+++ scripts/install_mhcnuggetsr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script to install mhcnuggetsr and its dependencies
+# on the Peregrine computer cluster
+#
+# Usage, locally:
+#
+#   ./install_mhcnuggetsr.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./install_mhcnuggetsr.sh
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=install_mhcnuggetsr
+#SBATCH --output=install_mhcnuggetsr.log
+module load R
+
+Rscript -e 'remotes::install_github("richelbilderbeek/mhcnuggetsr")'
+
+./install_mhcnuggets.sh
+
+
