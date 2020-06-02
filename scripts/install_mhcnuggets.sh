645440492fed6a6f87fa6fe65555e6330bf6447b
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-02 07:49:06 +0200

    Do upgrade pip when install mhcnuggets

--- scripts/install_mhcnuggets.sh
+++ scripts/install_mhcnuggets.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Script to install MHCnuggets
 # on the Peregrine computer cluster
 #
@@ -19,6 +20,8 @@
 #SBATCH --job-name=install_mhcnuggets
 #SBATCH --output=install_mhcnuggets.log
 module load R Python/3.8.2-GCCcore-9.3.0 binutils
+python -m pip install --upgrade pip --user
+python -m pip install ~/.local/share/mhcnuggets/ --user
 
 Rscript -e 'if (!mhcnuggetsr::is_mhcnuggets_installed()) mhcnuggetsr::install_mhcnuggets()'
 
--- scripts/install_mhcnuggetsr.sh
+++ scripts/install_mhcnuggetsr.sh
@@ -26,38 +26,3 @@ Rscript -e 'remotes::install_github("richelbilderbeek/mhcnuggetsr")'
 
 Rscript -e 'mhcnuggetsr::mhcnuggetsr_self_test()'
 
-echo "If the self-test failed, so this on Peregrine:"
-echo " "
-echo "module load R"
-echo "module load Python/3.8.2-GCCcore-9.3.0"
-echo "R"
-echo " "
-echo "(now you are in R)"
-echo " "
-echo "library(mhcnuggetsr)"
-echo "mhcnuggetsr_self_test()"
-echo " "
-echo "You will get the following message:"
-echo " "
-echo "  No non-system installation of Python could be found."
-echo "  Would you like to download and install Miniconda?"
-echo "  Miniconda is an open source environment management system for Python."
-echo "  See https://docs.conda.io/en/latest/miniconda.html for more details."
-echo "  "
-echo "  Would you like to install Miniconda? [Y/n]:"
-echo " "
-echo "Pick yes to install Miniconda"
-echo " "
-echo " "
-echo "Also try this from a terminal:"
-echo " "
-echo "module load Python/3.8.2-GCCcore-9.3.0"
-echo "python -m pip install --upgrade pip --user"
-echo "python -m pip install ~/.local/share/mhcnuggets/ --user"
-
-# 
-# > install_mhcnuggets()
-# ERROR: Could not install packages due to an EnvironmentError: [('/home/p230198/.local/share/mhcnuggets/.git/objects/pack/pack-1dd355527f837650a21ec9aa3fa2249c929b314b.pack', '/local/tmp/pip-req-build-vtoekd4p/.git/objects/pack/pack-1dd355527f837650a21ec9aa3fa2249c929b314b.pack', "[Errno 13] Permission denied: '/local/tmp/pip-req-build-vtoekd4p/.git/objects/pack/pack-1dd355527f837650a21ec9aa3fa2249c929b314b.pack'"), ('/home/p230198/.local/share/mhcnuggets/.git/objects/pack/pack-1dd355527f837650a21ec9aa3fa2249c929b314b.idx', '/local/tmp/pip-req-build-vtoekd4p/.git/objects/pack/pack-1dd355527f837650a21ec9aa3fa2249c929b314b.idx', "[Errno 13] Permission denied: '/local/tmp/pip-req-build-vtoekd4p/.git/objects/pack/pack-1dd355527f837650a21ec9aa3fa2249c929b314b.idx'")]
-
-
-
