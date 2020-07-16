fcdf40779ce03b99c26e87b2c18a421b9a3866d5
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-16 10:40:36 +0200

    'pirouette' depends on 'tiebeaur'

--- scripts/install_pirouette.sh
+++ scripts/install_pirouette.sh
@@ -25,7 +25,7 @@ module load HDF5/1.10.1-foss-2018a
 
 Rscript -e 'remotes::install_github("thijsjanzen/nodeSub")'
 Rscript -e 'remotes::install_github("Giappo/mbd")'
-
+Rscript -e 'remotes::install_github("richelbilderbeek/tiebeaur")'
 Rscript -e 'remotes::install_github("richelbilderbeek/pirouette")'
 
 
