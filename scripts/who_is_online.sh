1166c1d1359ad0a1a62a63d9c2c9f24edd5c1ae7
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-30 09:07:26 +0200

    Also look for p numbers

--- scripts/who_is_online.sh
+++ scripts/who_is_online.sh
@@ -6,4 +6,4 @@
 #
 #   ./who_is_online.sh
 #
-finger $(top -bc -n 1 | cut -d " " -f 2 | sort -u | egrep "(s|d)[[:digit:]]{7}") | egrep "Name: .*$"
+finger $(top -bc -n 1 | cut -d " " -f 2 | sort -u | egrep "(s|p)[[:digit:]]{6,7} ") | egrep "Name: .*$"
