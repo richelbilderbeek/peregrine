907bdc70e0d9c43069af088737beeb55cbb9a708
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-14 20:49:20 +0200

    Use module imagemagick

--- scripts/install_pirouette.sh
+++ scripts/install_pirouette.sh
@@ -18,7 +18,7 @@
 #SBATCH --mem=1G
 #SBATCH --job-name=install_pirouette
 #SBATCH --output=install_pirouette.log
-module load R
+module load R ImageMagick
 module load HDF5/1.10.1-foss-2018a
 
 ./install_mcbette.sh
