8d59feca12faebf7dd516cd7f5e7bb4ef5420b7f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-28 11:07:06 +0200

    Add 'install_msa'

---
+++ scripts/install_msa.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script to install the msa Bioconductor package
+# and its dependencies
+# on the Peregrine computer cluster
+#
+# Usage, locally:
+#
+#   ./install_msa.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./install_msa.sh
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=install_msa
+#SBATCH --output=install_msa.log
+module load R
+
+Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")'
+Rscript -e 'BiocManager::install("msa")'
+
