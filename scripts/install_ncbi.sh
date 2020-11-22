cd94704dbc034d4359791f69ccff37c9c337ec13
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-22 11:08:39 +0100

    Add 'install_ncbi'

---
+++ scripts/install_ncbi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script to install ncbi and its dependencies
+# on the Peregrine computer cluster
+#
+# Usage, locally:
+#
+#   ./install_ncbi.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./install_ncbi.sh
+#
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=install_ncbi
+#SBATCH --output=install_ncbi.log
+module load R
+
+Rscript -e 'remotes::install_github("richelbilderbeek/ncbi")'
+
+
