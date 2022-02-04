dc96ad8241b3ffa96550bde4cafe063eba37c3c6
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-04 12:13:11 +0100

    EOD

--- doc/jobs_in_sequence/run.sh
+++ doc/jobs_in_sequence/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start job1, store job ID
-jobid=`sbatch job1.sh | cut -d ' ' -f 4`
+jobid=$(sbatch job1.sh | cut -d ' ' -f 4)
 
 # Start job2 after job1
 sbatch --dependency=afterok:$jobid job2.sh
