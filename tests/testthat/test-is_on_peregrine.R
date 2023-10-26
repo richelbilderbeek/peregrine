f3044ac8ed17782be172426da4fbab0c42941359
richelbilderbeek <richel@richelbilderbeek.nl>
2023-10-26 11:57:25 +0200

    No depend on assertive

--- tests/testthat/test-is_on_peregrine.R
+++ tests/testthat/test-is_on_peregrine.R
@@ -1,7 +1,7 @@
 test_that("use", {
   # Cannot check if this is TRUE or FALSE: it depends
   # where the test if run
-  expect_true(assertive::is_if_condition(is_on_peregrine()))
+  expect_true(beautier::is_one_bool(is_on_peregrine()))
 
   # Travis is not Peregrine
   if (beautier::is_on_travis()) {
--- tests/testthat/test-is_on_peregrine_worker_node.R
+++ tests/testthat/test-is_on_peregrine_worker_node.R
@@ -1,7 +1,7 @@
 test_that("use", {
   # Cannot check if this is TRUE or FALSE: it depends
   # where the test if run
-  expect_true(assertive::is_if_condition(is_on_peregrine_worker_node()))
+  expect_true(beautier::is_one_bool(is_on_peregrine_login_node()))
 
   # Travis is not Peregrine
   if (beautier::is_on_travis()) {
